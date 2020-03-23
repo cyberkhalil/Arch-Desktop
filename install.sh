d8498e40d2d246bb4762ec64b5ed2778cef0de74
Mahmoud khalil <khalil2535@protonmail.com>
2020-03-23 17:27:30 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+pacman -Syu --noconfirm
+
+# basics & drivers
+pacman -S alsa-plugins alsa-firmware dosfstools efibootmgr elisa ffmpeg grub linux-headers linux-lts linux-lts-headers man mesa powerdevil pulseaudio pulseaudio-alsa pulseaudio-qt sudo vim
+
+# KDE sweets
+pacman -S akregator ark dolphin dolphin-plugins kgpg gwenview kaddressbook kate kontact knotes konsole kdeconnect kdenetwork-meta kdenlive kexi kfind kmag kmail latte-dock plasma-nm okular plasma-desktop wayland
+
+# yay !
+pacman -S git --noconfirm
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+
+# wireless
+yay -S networkmanager aircrack-ng rtl8821ce-dkms-git speedtest-cli wifite2-git wireshark-qt wifite
+
+# others
+yay -S libreoffice-fresh flameshot grub-customizer neofetch youtube-dl vlc virtualbox lbry-app-bin nextcloud-client zaproxy telegram-desktop timeshift cronie qalculate-gtk
+
+# programming
+intellij-idea-community-edition netbeans python
+
+# browsers
+yay -S brave-bin chromium firefox
+
+# fonts
+noto-fonts noto-fonts-emoji ttf-bitstream-vera ttf-dejavu ttf-droid ttf-hack ttf-inconsolata ttf-indic-otf ttf-liberation ttf-ms-fonts ttf-vista-fonts
